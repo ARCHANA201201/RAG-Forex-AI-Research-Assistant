2e196ef698d38e9d60f15956e3fc6c116b7d849a
unknown <archanaajith@gmail.com>
2026-03-14 14:04:30 +0530

    Fix LangChain chains import

--- rag.py
+++ rag.py
@@ -8,7 +8,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_groq import ChatGroq
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain.chains import ConversationalRetrievalChain
+from langchain_community.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 
 load_dotenv()
